ff11c51991cec476ce1702180337d5817289024b
Gowtham <0622gowtham@gmail.com>
2023-04-21 12:20:18 +0530

    changes in a

--- bs2.java
+++ bs2.java
@@ -3,7 +3,7 @@ package firstproject;
 public class bs2 {
     public static class bs1 {
         public static void main(String[] args) {
-            int a =30;
+            int a =10;
             int b = 20;
             int sum = a+b;
             System.out.println(sum);
